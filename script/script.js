644f41f8ac269f5e73caf8014aa5b7700d4063f7
facundohsilva <103458342+facundohsilva@users.noreply.github.com>
2023-01-19 17:21:56 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var texto1, texto2=""
+function cambiarInfoPersonal(texto1,texto2){
+    document.getElementById("desInfoPersonal").innerHTML= texto1;
+    document.getElementById("infoPersonalTexto").innerHTML= texto2;
+}
